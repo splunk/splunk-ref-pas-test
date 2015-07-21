a0f47368e3cd52d778a2c85fd975e29022dd7a9f
Brian Schutz <schutzdev@gmail.com>
2015-07-21 12:15:09 -0500

    Revert "Simulated Account Lock/Unlock"
    This reverts commit 8f9134820ca16b82204412bd9bb4c85aafaa5d0e.

--- pas_simulated_users_addon/user-api/app.py
+++ pas_simulated_users_addon/user-api/app.py
@@ -21,34 +21,31 @@ users = [
         'CompanyAddress': u'123 Main St, Austin, TX - 78737', 
         'CompanyPhone': u'512-808-0808',
         'UserImage': u'No Image Available Yet',
-        'done': False,
-        'AccountStatus': 'Account Unlocked'
+        'done': False
     },
-    {
-        'UserName': 'jbell',
-        'UserFullName': u'John Bell',
-        'UserEmail': u'jbell@ibm.us.com', 
-        'UserPhone': u'308-777-999', 
-        'CompanyName': u'IBM US, Inc.', 
-        'UserRole': u'Systems Engineer', 
-        'CompanyAddress': u'16 Elm Street, Charlotee, NC- 87699', 
-        'CompanyPhone': u'876-808-0808',
-        'UserImage': 'No Image Available Yet',
-        'done': False,
-        'AccountStatus': 'Account Unlocked'
+     {
+            'UserName': 'jbell',
+            'UserFullName': u'John Bell',
+            'UserEmail': u'jbell@ibm.us.com', 
+            'UserPhone': u'308-777-999', 
+            'CompanyName': u'IBM US, Inc.', 
+            'UserRole': u'Systems Engineer', 
+            'CompanyAddress': u'16 Elm Street, Charlotee, NC- 87699', 
+            'CompanyPhone': u'876-808-0808',
+            'UserImage': 'No Image Available Yet',
+            'done': False
     },
-    {
-        'UserName': 'bberry',
-        'UserFullName': u'Ben Berry',
-        'UserEmail': u'bberry@gmail.com', 
-        'UserPhone': u'208-999-999', 
-        'CompanyName': u'Splunk, Inc.', 
-        'UserRole': u'Developer', 
-        'CompanyAddress': u'61615 5th street, Austin, TX - 78737', 
-        'CompanyPhone': u'512-677-9999',
-        'UserImage': 'No Image Available Yet',
-        'done': False,
-        'AccountStatus': 'Account Unlocked'
+     {
+            'UserName': 'bberry',
+            'UserFullName': u'Ben Berry',
+            'UserEmail': u'bberry@gmail.com', 
+            'UserPhone': u'208-999-999', 
+            'CompanyName': u'Splunk, Inc.', 
+            'UserRole': u'Developer', 
+            'CompanyAddress': u'61615 5th street, Austin, TX - 78737', 
+            'CompanyPhone': u'512-677-9999',
+            'UserImage': 'No Image Available Yet',
+            'done': False
     }
 ]
 
@@ -63,35 +60,13 @@ def get_user(user_name):
         abort(404)
     return jsonify({'user': user[0]})
 
-@app.route('/user_list/api/v1.0/users/lock/<string:user_name>', methods=['POST'])
-def lock_user_account(user_name):
-    try:
-        user = filter(lambda t: t['UserName'] == user_name, users)
-        user[0]["AccountStatus"] = 'Account Locked'
-
-        if len(user) == 0:
-            abort(404)
-        return jsonify({'user': user[0]["AccountStatus"]})
-    except Exception, e:
-        print >> sys.stderr, "ERROR Error sending message: %s" % e
-        return jsonify({'Error': "Account lock attempt failed!"})
-
-@app.route('/user_list/api/v1.0/users/unlock/<string:user_name>', methods=['POST'])
-def unlock_user_account(user_name):
-    try:
-        user = filter(lambda t: t['UserName'] == user_name, users)
-        user[0]["AccountStatus"] = 'Account Unlocked'
-
-        if len(user) == 0:
-            abort(404)
-        return jsonify({'user': user[0]["AccountStatus"]})
-    except Exception, e:
-        print >> sys.stderr, "ERROR Error sending message: %s" % e
-        return jsonify({'Error': "Account lock attempt failed!"})
-
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+
